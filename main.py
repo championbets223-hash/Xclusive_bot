7c131af7239397ed45ff44352febcd3b9f58e235
championbets223-hash <championbets223@gmail.com>
2026-05-26 17:31:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("856a377f205f826e152f26e8811273ab")
 BOT_TOKEN = os.getenv("8870716064:AAEmirIH2LjcnyRqAxzuQvdXhoCQ-wBE-KQ")
 
-SOURCE_CHANNEL = int(os.getenv("-1001321798756"))  # Channel B ID
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL")) # Channel B ID
 TARGET_CHANNEL = int(os.getenv("-1001331429198"))  # Channel A ID
 WEBSITE_LINK = os.getenv("xclusivelive.netlify.app")           # Your VIP link
 
